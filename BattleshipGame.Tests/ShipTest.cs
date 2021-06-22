27bbaa73e8ec2b128445cb85d22272e1c2c64e11
dmitry89 <dmitry@solcast.com.au>
2021-06-22 13:59:50 +1000

    test: formatting

--- BattleshipGame.Tests/ShipTest.cs
+++ BattleshipGame.Tests/ShipTest.cs
@@ -10,14 +10,16 @@ namespace BattleshipGame.Tests
         public void Ship_Of_Size_2_Hit_Once_Should_Be_Alive() => MakeShip()
             .Hit(0, 0)
             .IsDead
-            .Should().Be(false);
+            .Should()
+            .Be(false);
 
         [Test]
         public void Ship_Of_Size_2_Hit_Twice_Should_Be_Dead() => MakeShip()
             .Hit(0, 0)
             .Hit(1, 0)
             .IsDead
-            .Should().Be(true);
+            .Should()
+            .Be(true);
 
         private Ship MakeShip() => new Ship(0, 0, Direction.Horizontal, 2);
     }
